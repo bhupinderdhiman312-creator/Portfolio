4745cd7ea83ad212000e7544d4fd96a428a9a35a
Bhupinder Singh <bhupinderdhiman312@gmail.com>
2026-03-16 15:22:40 +0530

    make a div

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,8 @@ function About() {
   }
 
   return (
-    <div className='h-screen w-full  flex flex-col justify-evenly items-center bg-gray-950'>
+    <>
+    <div className='h-250 w-full  flex flex-col justify-evenly items-center bg-gray-950'>
       <motion.div
         initial={{ opacity: 5, y:70}}
         animate={{ opacity: 1, y: 0}}
@@ -50,7 +51,46 @@ function About() {
     
     </motion.div>
     </div>
+    <motion.div
+        className="flex flex-col md:flex-row flex-wrap justify-center items-center gap-6 mt-6 md:mt-10 px-2 md:px-0 w-full text-white"
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true, amount: 0.2 }}
+      >
+        <div className='h-62 w-full flex justify-evenly items-center'>
+        <div className="h-55 w-64 sm:w-72 md:w-80 text-center hover:translate-y-2 shadow-md hover:shadow-indigo-500/40 transition-all duration-300 ease-in-out rounded-2xl flex flex-col gap-6 border border-sky-200 p-4">
+          <p className="font-bold">HTML</p>
+          <p>
+            Strong understanding of semantic HTML5 for creating well-structured,
+            accessible, and SEO-friendly web pages.
+          </p>
+        </div>
+        <div className="h-55 w-64 sm:w-72 md:w-80 text-center hover:translate-y-2 shadow-md hover:shadow-indigo-500/40 transition-all duration-300 ease-in-out rounded-2xl flex flex-col gap-6 border border-sky-200 p-4">
+          <p className="font-bold">CSS</p>
+          <p>
+            Experienced in styling responsive layouts using CSS3, Flexbox, Grid,
+            and modern frameworks like Tailwind CSS
+          </p>
+        </div>
+        <div className="h-55 w-64 sm:w-72 md:w-80 text-center hover:translate-y-2 shadow-md hover:shadow-indigo-500/40 transition-all duration-300 ease-in-out rounded-2xl flex flex-col gap-6 border border-sky-200 p-4">
+          <p className="font-bold">JAVASCRIPT</p>
+          <p>
+            Proficient in JavaScript (ES6+) for building interactive features,
+            handling DOM manipulation, and working with APIs.
+          </p>
+        </div>
+        <div className="h-55 w-64 sm:w-72 md:w-80 text-center hover:translate-y-2 shadow-md hover:shadow-indigo-500/40 transition-all duration-300 ease-in-out rounded-2xl flex flex-col gap-6 border border-sky-200 p-4">
+          <p className="font-bold">REACT.JS</p>
+          <p>
+            Skilled in developing dynamic single-page applications using
+            React.js, including hooks, components, and state management.
+          </p>
+        </div>
+        </div>
+      </motion.div>
     </div>
+    </>
   )
 }
 
